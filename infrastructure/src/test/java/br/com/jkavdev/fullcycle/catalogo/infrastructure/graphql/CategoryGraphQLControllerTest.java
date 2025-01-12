7f1287f4ff9daa81fd652b506b01f5ab8efe6307
jkavdev <jhonatankolen@gmail.com>
2025-01-12 19:26:45 -0300

    Full Cycle 3.0 - Microsserviço: API do Catálogo com Java ( Back-end ) - 0408 - ajustando arquivos de configuracao e testes unitarios
    - removendo configuracao do ObjectMapperConfig, pois o spring ja faz isso injetando o jsonComponent, ai apenas indicar que ObjectMapperConfig eh um JsonComponent
    
    - informando o controller de cada teste do grapqh para que seja disponibilizado apenas um controller
    
    - e utilizando a passagem de variaveis para a consulta em vez de interpolacao

--- infrastructure/src/test/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/graphql/CategoryGraphQLControllerTest.java
+++ infrastructure/src/test/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/graphql/CategoryGraphQLControllerTest.java
@@ -17,7 +17,9 @@ import org.springframework.graphql.test.tester.GraphQlTester;
 
 import java.util.List;
 
-@GraphQLControllerTest
+// quando indicamos o controller, sera disponibilizado apenas o controle a ser testado
+// senao seria disponibilizado todos os controlers disponiveis
+@GraphQLControllerTest(controllers = CategoryGraphQLController.class)
 public class CategoryGraphQLControllerTest {
 
     @MockBean
@@ -102,6 +104,62 @@ public class CategoryGraphQLControllerTest {
 
         // when
         final var res = graphql.document(query).execute();
+
+        final var actualCategories = res.path("categories")
+                .entityList(ListCategoryOutput.class)
+                .get();
+
+        // then
+        Assertions.assertTrue(
+                expectedCategories.size() == actualCategories.size()
+                        && expectedCategories.containsAll(actualCategories)
+        );
+
+        final var captor = ArgumentCaptor.forClass(CategorySearchQuery.class);
+        Mockito.verify(listCategoryUseCase, Mockito.times(1)).execute(captor.capture());
+
+        final var actualQuery = captor.getValue();
+        Assertions.assertEquals(expectedPage, actualQuery.page());
+        Assertions.assertEquals(expectedPerPage, actualQuery.perPage());
+        Assertions.assertEquals(expectedSort, actualQuery.sort());
+        Assertions.assertEquals(expectedDirection, actualQuery.direction());
+        Assertions.assertEquals(expectedSearch, actualQuery.terms());
+    }
+
+    @Test
+    public void givenCustomArgumentsWithVariablesWhenCallsListCategoriesShouldReturn() {
+        // given
+        final var expectedCategories = List.of(
+                ListCategoryOutput.from(Fixture.Categories.aulas()),
+                ListCategoryOutput.from(Fixture.Categories.lives())
+        );
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedSort = "name";
+        final var expectedDirection = "asc";
+        final var expectedSearch = "";
+
+        Mockito.when(listCategoryUseCase.execute(ArgumentMatchers.any()))
+                .thenReturn(new Pagination<>(expectedPage, expectedPerPage, expectedCategories.size(), expectedCategories));
+
+        final var query = """
+                query AllCategories($search: String, $page: Int, $perPage: Int, $sort: String, $direction: String){
+                
+                    categories(search: $search, page: $page, perPage: $perPage, sort: $sort, direction: $direction) {
+                      id
+                      name
+                    }
+                }
+                """;
+
+        // when
+        final var res = graphql.document(query)
+                .variable("search", expectedSearch)
+                .variable("page", expectedPage)
+                .variable("perPage", expectedPerPage)
+                .variable("sort", expectedSort)
+                .variable("direction", expectedDirection)
+                .execute();
 
         final var actualCategories = res.path("categories")
                 .entityList(ListCategoryOutput.class)
