c42f537708f86a39a7723313e86d80973adee815
jkavdev <jhonatankolen@gmail.com>
2025-01-14 00:58:30 -0300

    Full Cycle 3.0 - Microsserviço: API do Catálogo com Java ( Back-end ) - 0601 - adicionando document de categoria

--- infrastructure/src/main/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/category/CategoryRepository.java
+++ infrastructure/src/main/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/category/CategoryRepository.java
@@ -1,5 +1,6 @@
 package br.com.jkavdev.fullcycle.catalogo.infrastructure.category;
 
+import br.com.jkavdev.fullcycle.catalogo.infrastructure.category.persistence.CategoryDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface CategoryRepository extends ElasticsearchRepository<CategoryDocument, String> {
