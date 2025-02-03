7ff27a8cda80f929c6005f1b0b715050b5dacebb
jkavdev <jhonatankolen@gmail.com>
2025-02-03 02:17:57 -0300

    Full Cycle 3.0 - Microsserviço: API do Catálogo com Java ( Back-end ) - 0915 - teste de regressao completo entre mimcroservicos com alguns fixes - mas ainda com o erro do kafka connect

--- infrastructure/src/main/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/configuration/WebServerConfig.java
+++ infrastructure/src/main/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -1,11 +1,27 @@
 package br.com.jkavdev.fullcycle.catalogo.infrastructure.configuration;
 
+import br.com.jkavdev.fullcycle.catalogo.infrastructure.authentication.RefreshClientCredencials;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration(proxyBeanMethods = false)
 @ComponentScan("br.com.jkavdev.fullcycle.catalogo")
 @EnableScheduling
 public class WebServerConfig {
+
+    @Bean
+    @Profile("!test-integration & !test-e2e")
+    ApplicationListener<ContextRefreshedEvent> refreshClientCrendentials(
+            final RefreshClientCredencials refreshClientCredencials
+    ) {
+        return event -> {
+            refreshClientCredencials.refresh();
+        };
+    }
+
 }
