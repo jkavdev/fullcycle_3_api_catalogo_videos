a02de32bef2e30281e69d1cbbeba2e1f80c2dc79
jkavdev <jhonatankolen@gmail.com>
2025-02-04 23:36:53 -0300

    Full Cycle 3.0 - Microsserviço: API do Catálogo com Java ( Back-end ) - 1003 - criando a interface de gateway de cast member

---
+++ domain/src/main/java/br/com/jkavdev/fullcycle/catalogo/domain/castmember/CastMemberGateway.java
@@ -0,0 +1,19 @@
+package br.com.jkavdev.fullcycle.catalogo.domain.castmember;
+
+import br.com.jkavdev.fullcycle.catalogo.domain.category.Category;
+import br.com.jkavdev.fullcycle.catalogo.domain.category.CategorySearchQuery;
+import br.com.jkavdev.fullcycle.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CastMemberGateway {
+
+    CastMember save(CastMember aMember);
+
+    void deleteById(String anId);
+
+    Optional<CastMember> findById(String anId);
+
+    Pagination<CastMember> findAll(CastMemberSearchQuery aQuery);
+
+}
