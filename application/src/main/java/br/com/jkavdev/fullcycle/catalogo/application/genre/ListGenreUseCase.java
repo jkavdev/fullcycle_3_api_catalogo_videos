6168e714b56368185688b8db50b3c77d47712ad2
jkavdev <jhonatankolen@gmail.com>
2025-02-07 01:53:28 -0300

    Full Cycle 3.0 - Microsserviço: API do Catálogo com Java ( Back-end ) - 1407 - criando o caso de uso de listagem de genre

---
+++ application/src/main/java/br/com/jkavdev/fullcycle/catalogo/application/genre/ListGenreUseCase.java
@@ -0,0 +1,56 @@
+package br.com.jkavdev.fullcycle.catalogo.application.genre;
+
+import br.com.jkavdev.fullcycle.catalogo.application.UseCase;
+import br.com.jkavdev.fullcycle.catalogo.domain.genre.Genre;
+import br.com.jkavdev.fullcycle.catalogo.domain.genre.GenreGateway;
+import br.com.jkavdev.fullcycle.catalogo.domain.genre.GenreSearchQuery;
+import br.com.jkavdev.fullcycle.catalogo.domain.pagination.Pagination;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class ListGenreUseCase extends UseCase<ListGenreUseCase.Input, Pagination<ListGenreUseCase.Output>> {
+
+    private final GenreGateway genreGateway;
+
+    public ListGenreUseCase(final GenreGateway genreGateway) {
+        this.genreGateway = Objects.requireNonNull(genreGateway);
+    }
+
+    @Override
+    public Pagination<Output> execute(final Input input) {
+        final var aQuery = new GenreSearchQuery(
+                input.page(),
+                input.perPage(),
+                input.terms(),
+                input.sort(),
+                input.direction(),
+                input.categories()
+        );
+
+        return genreGateway.findAll(aQuery)
+                .map(Output::from);
+    }
+
+    public record Input(
+            int page,
+            int perPage,
+            String terms,
+            String sort,
+            String direction,
+            Set<String> categories
+    ) {
+
+    }
+
+    public record Output(
+            String id,
+            String name
+    ) {
+
+        public static Output from(final Genre genre) {
+            return new Output(genre.id(), genre.name());
+        }
+    }
+
+}
