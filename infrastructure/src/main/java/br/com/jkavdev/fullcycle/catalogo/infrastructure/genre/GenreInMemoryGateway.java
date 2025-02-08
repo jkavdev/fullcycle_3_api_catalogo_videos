a32128f92be901433fa0e04811946974443d5664
jkavdev <jhonatankolen@gmail.com>
2025-02-07 23:47:43 -0300

    Full Cycle 3.0 - Microsserviço: API do Catálogo com Java ( Back-end ) - 1501 - criando o schema do graphql e o genre gateway em memoria

---
+++ infrastructure/src/main/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/genre/GenreInMemoryGateway.java
@@ -0,0 +1,44 @@
+package br.com.jkavdev.fullcycle.catalogo.infrastructure.genre;
+
+import br.com.jkavdev.fullcycle.catalogo.domain.genre.Genre;
+import br.com.jkavdev.fullcycle.catalogo.domain.genre.GenreGateway;
+import br.com.jkavdev.fullcycle.catalogo.domain.genre.GenreSearchQuery;
+import br.com.jkavdev.fullcycle.catalogo.domain.pagination.Pagination;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class GenreInMemoryGateway implements GenreGateway {
+
+    private final ConcurrentHashMap<String, Genre> db;
+
+    public GenreInMemoryGateway() {
+        this.db = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public Genre save(Genre aGenre) {
+        this.db.put(aGenre.id(), aGenre);
+        return aGenre;
+    }
+
+    @Override
+    public void deleteById(String anId) {
+        db.remove(anId);
+    }
+
+    @Override
+    public Optional<Genre> findById(String anId) {
+        return Optional.ofNullable(db.get(anId));
+    }
+
+    @Override
+    public Pagination<Genre> findAll(GenreSearchQuery aQuery) {
+        final var values = db.values();
+        return new Pagination<>(aQuery.page(), aQuery.perPage(), values.size(), new ArrayList<>(values));
+    }
+
+}
